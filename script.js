b01aa891f0e798dec47e51fdbf59189e9fd37f20
Magooch <crvallecito@gmail.com>
2024-06-21 11:44:42 -0600

    Assign the values of myName and myAge to short variables inside dogCalculator

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ let out = document.querySelector('.output')
 let err = document.querySelector('.error')
 
 function dogCalculator() {
-    if (myName.value == "" || myAge.value < 1) {
+
+    let n = myName.value;
+    let a = myAge.value;
+
+    if (n == "" || a < 1) {
         err.textContent = "Please enter a name and/or age starting from 1"
     } else {
         let earlyYears = 2 * 10.5
         let laterYears = myAge.value - 2
         laterYears = laterYears * 4
         let dogYears = earlyYears + laterYears
-        out.textContent = `Hello ${myName.value}, you are ${dogYears} years old in dog years!`
+        out.textContent = `Hello ${n}, you are ${dogYears} years old in dog years!`
     }
 }
 
