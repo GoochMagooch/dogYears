1afa2b001929513c1d8a7d22978f039118cb3fc4
Magooch <crvallecito@gmail.com>
2024-06-21 13:37:09 -0600

    Replace 'let' with 'const' for variables

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let myName = document.getElementById('name')
-let myAge = document.getElementById('age')
-let btn = document.getElementById('btn')
-let out = document.querySelector('.output')
-let err = document.querySelector('.error')
+const myName = document.getElementById('name')
+const myAge = document.getElementById('age')
+const btn = document.getElementById('btn')
+const out = document.querySelector('.output')
+const err = document.querySelector('.error')
 
 function dogCalculator() {
 
-    let name = myName.value;
-    let age = myAge.value;
+    const name = myName.value;
+    const age = myAge.value;
 
     if (name == "" || age < 1) {
         err.textContent = "Please enter your name and/or age, starting from 1"
     } else {
-        let earlyYears = 2 * 10.5
-        let laterYears = myAge.value - 2
+        const earlyYears = 2 * 10.5
+        const laterYears = myAge.value - 2
         laterYears = laterYears * 4
-        let dogYears = earlyYears + laterYears
+        const dogYears = earlyYears + laterYears
         out.textContent = `Hello ${name}, you are ${dogYears} years old in dog years!`
         err.textContent = ""
     }
